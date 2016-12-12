cbd2c08ad832adc29632fd41232594670c1f9148
Audeon Nicolas <dev@naudeon.xyz>
2016-12-12 16:46:07 +0100

    First commit of rental core plugin
    Change-Id: I16a06d8c6fbc5a454c91bbc9013a3e7c7ae07ad2

---
+++ com.opti.rental.core/src/com/opti/rental/core/RentalCoreActivator.java
@@ -0,0 +1,40 @@
+package com.opti.rental.core;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import com.opcoach.training.rental.RentalAgency;
+import com.opcoach.training.rental.helpers.RentalAgencyGenerator;
+
+public class RentalCoreActivator implements BundleActivator {
+
+	private static BundleContext context;
+
+	private static RentalAgency agency;
+	
+	static BundleContext getContext() {
+		return context;
+	}
+
+	static RentalAgency getAgency() {
+		return agency;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		RentalCoreActivator.context = bundleContext;
+		RentalCoreActivator.agency = RentalAgencyGenerator.createSampleAgency();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		RentalCoreActivator.context = null;
+	}
+	
+}
