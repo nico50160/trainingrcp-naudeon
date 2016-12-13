0b69a91a44e7c67358db50e323ce76ab90f5fba7
Audeon Nicolas <dev@naudeon.xyz>
2016-12-13 17:19:48 +0100

    Ajout de la perspective

---
+++ com.opti.rental.ui/src/com/opti/rental/ui/perspective/RentalPerspective.java
@@ -0,0 +1,43 @@
+package com.opti.rental.ui.perspective;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IFolderLayout;
+
+public class RentalPerspective implements IPerspectiveFactory {
+
+	/**
+	 * Creates the initial layout for a page.
+	 */
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		String editorArea = layout.getEditorArea();
+		addFastViews(layout);
+		addViewShortcuts(layout);
+		addPerspectiveShortcuts(layout);
+		{
+			IFolderLayout folderLayout = layout.createFolder("folder", IPageLayout.LEFT, 0.5f, IPageLayout.ID_EDITOR_AREA);
+			folderLayout.addView("com.opti.rental.ui.RentalTreeView");
+		}
+		layout.addView("com.opti.rental.ui.propertyView", IPageLayout.BOTTOM, 0.5f, "folder");
+	}
+
+	/**
+	 * Add fast views to the perspective.
+	 */
+	private void addFastViews(IPageLayout layout) {
+	}
+
+	/**
+	 * Add view shortcuts to the perspective.
+	 */
+	private void addViewShortcuts(IPageLayout layout) {
+	}
+
+	/**
+	 * Add perspective shortcuts to the perspective.
+	 */
+	private void addPerspectiveShortcuts(IPageLayout layout) {
+	}
+
+}
