51871d6bdbb3f2914525de849a7fc0283f6dac15
Audeon Nicolas <dev@naudeon.xyz>
2016-12-13 13:19:30 +0100

    Ajout icone sur les noeuds intérmédiaires et  changement des couleurs (passage à des couleurs pastel)

--- com.opti.rental.ui/src/com/opti/rental/ui/providers/RentalProvider.java
+++ com.opti.rental.ui/src/com/opti/rental/ui/providers/RentalProvider.java
@@ -2,14 +2,22 @@ package com.opti.rental.ui.providers;
 
 import java.util.List;
 
+import org.eclipse.jface.viewers.IColorProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 
 import com.opcoach.training.rental.Customer;
+import com.opcoach.training.rental.Rental;
 import com.opcoach.training.rental.RentalAgency;
 import com.opcoach.training.rental.RentalObject;
+import com.opti.rental.ui.RentalUIActivator;
+import com.opti.rental.ui.RentalUIConstants;
 
-public class RentalProvider extends LabelProvider implements ITreeContentProvider {
+public class RentalProvider extends LabelProvider implements ITreeContentProvider, IColorProvider, RentalUIConstants {
 	
 	private static final Object[] EMPTY_RESULT = new Object[0];
 	
@@ -31,13 +39,11 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 	@Override
 	public Object[] getChildren(Object parentElement) {
 		Object[] result = null;
-		
 		if (parentElement instanceof RentalAgency) {
 			result = new Node[] {new Node(NodeType.CUSTOMER, (RentalAgency) parentElement), new Node(NodeType.LOCATION, (RentalAgency) parentElement), new Node(NodeType.RENTAL_OBJECT, (RentalAgency) parentElement)};
 		} else if (parentElement instanceof Node) {
 			result = ((Node) parentElement).getChildren();
 		}
-		
 		return (null == result) ? EMPTY_RESULT : result;
 	}
 
@@ -63,6 +69,43 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 		} 
 		return super.getText(element);
 	}
+
+	@Override
+	public Color getForeground(Object element) {
+		Color result = null;
+		if (element instanceof Rental) {
+			result = Display.getCurrent().getSystemColor(SWT.COLOR_DARK_BLUE);
+		} else if (element instanceof Customer) {
+			result = Display.getCurrent().getSystemColor(SWT.COLOR_DARK_CYAN);
+		} else if (element instanceof RentalObject) {
+			result = Display.getCurrent().getSystemColor(SWT.COLOR_DARK_RED);
+		} 
+		return result;
+	}
+
+	@Override
+	public Color getBackground(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	@Override
+	public Image getImage(Object element) {
+		if (element instanceof Rental) {
+			return RentalUIActivator.getDefault().getImageRegistry().get(IMG_RENTAL);
+		} else if (element instanceof Customer) {
+			return RentalUIActivator.getDefault().getImageRegistry().get(IMG_CUSTOMER);
+		} else if (element instanceof RentalObject) {
+			return RentalUIActivator.getDefault().getImageRegistry().get(IMG_RENTAL_OBJECT);
+		} else if (element instanceof RentalAgency) {
+			return RentalUIActivator.getDefault().getImageRegistry().get(IMG_AGENCY);
+		} else if (element instanceof Node) {
+			return RentalUIActivator.getDefault().getImageRegistry().get(IMG_NODE);
+		}
+		return super.getImage(element);
+	}
+
 
 	public enum NodeType {
 		
@@ -117,4 +160,6 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 		
 		
 	}
+
+
 }
