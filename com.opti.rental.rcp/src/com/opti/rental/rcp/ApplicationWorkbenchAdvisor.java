72acb19b5d64f99578ce41fb4f0439b55e5dbccc
Audeon Nicolas <dev@naudeon.xyz>
2016-12-14 17:02:01 +0100

    Suite ajout RCP

---
+++ com.opti.rental.rcp/src/com/opti/rental/rcp/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,22 @@
+package com.opti.rental.rcp;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+import com.opti.rental.ui.perspective.RentalPerspective;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = RentalPerspective.ID; //$NON-NLS-1$
+
+	@Override
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+    
+    @Override
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
